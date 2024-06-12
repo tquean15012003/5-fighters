8635f93558f9cd759cc847bad225c1807db917fd
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-06-12 09:13:05 +0800

    fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,16 +13,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ["*"],
-  })
-);
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // app.use(
 //   session({
